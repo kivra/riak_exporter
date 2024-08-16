a39496c8d1a85db668f24df41475465c89c39aab
Silvio Sepulveda <silvio.sepulveda@kivra.com>
2024-08-16 11:59:28 +0200

    Add supoprt for riak repl stats

--- src/riak_exporter/__main__.py
+++ src/riak_exporter/__main__.py
@@ -12,6 +12,7 @@ def main():
     # Add some args config
     parser = argparse.ArgumentParser(description='Riak KV metrics exporter to Prometheus.')
     parser.add_argument('--riak', dest='riak_stats', help='url of the Riak KV stats endpoint')
+    parser.add_argument('--riak-repl', dest='riak_repl_stats', help='url of the Riak Repl stats endpoint')
     parser.add_argument('--address', dest='address', help='address to serve on')
     parser.add_argument('--port', dest='port', help='port to bind')
     parser.add_argument('--endpoint', dest='endpoint', help='Prometheus metrics endpoint location')
